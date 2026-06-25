04cfc9482a6bcb02659234161e73da61082011f1
quanldl <ldlq2005@gmail.com>
2026-06-26 04:15:49 +0700

    modified:   infra/modules/irsa/alb-controller.tf         modified:   infra/modules/vpc/subnet.tf

--- infra/modules/vpc/subnet.tf
+++ infra/modules/vpc/subnet.tf
@@ -6,7 +6,7 @@ resource "aws_subnet" "public" {
   map_public_ip_on_launch = true
   tags = merge(var.tags,{
     Name                     = "${var.project_name}-public-${each.key}"
-    "kubernetes.io/role/alb"  = "1"
+    "kubernetes.io/role/elb"  = "1"
   })
 }
 
@@ -19,6 +19,6 @@ resource "aws_subnet" "private" {
   map_public_ip_on_launch = false
   tags = merge(var.tags,{
     Name = "${var.project_name}-private-${each.key}"
-    "kubernetes.io/role/internal-lb" = "1"
+    "kubernetes.io/role/internal-alb" = "1"
 })
 }
