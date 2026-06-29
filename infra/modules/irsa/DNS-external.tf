38143b669b54dc743891a0cceb261fb15e073d20
quanldl <ldlq2005@gmail.com>
2026-06-30 01:56:10 +0700

    modified:   eks_core/eks.tf         modified:   irsa/DNS-external.tf         modified:   route53-ACM/main.tf

--- infra/modules/irsa/DNS-external.tf
+++ infra/modules/irsa/DNS-external.tf
@@ -15,7 +15,7 @@ resource "aws_iam_role" "externaldns_irsa" {
         Action = "sts:AssumeRoleWithWebIdentity",
         Condition = {
           StringEquals = {
-            "${local.oidc_host}:sub" = "system:serviceaccount:kube-system:${var.external_dns_sa}",
+            "${local.oidc_host}:sub" = "system:serviceaccount:external-dns:${var.external_dns_sa}",
             "${local.oidc_host}:aud" = "sts.amazonaws.com"
           }
         }
@@ -52,7 +52,7 @@ resource "aws_iam_policy" "externaldns_policy" {
           "route53:ListResourceRecordSets"
         ],
         # Resource = var.route53_zone_arns
-        Resource = ["arn:aws:route53:::hostedzone/*"]  
+        Resource = ["arn:aws:route53:::hostedzone/Z06648491EIV5VVWZPY0X"]  
       } 
     ]
   })
--- infra/modules/route53-ACM/main.tf
+++ infra/modules/route53-ACM/main.tf
@@ -1,5 +1,5 @@
 data "aws_route53_zone" "main" {
-  name = var.domain_name
+  name         = var.domain_name
   private_zone = false
 }
 
@@ -7,37 +7,38 @@ resource "aws_acm_certificate" "cert" {
   domain_name       = var.domain_name
   validation_method = "DNS"
 
-  subject_alternative_names = [ 
-    var.domain_name,
-    "*.${var.domain_name}" 
+  subject_alternative_names = [
+    "*.${var.domain_name}"
   ]
 
   lifecycle {
     create_before_destroy = true
   }
 }
-locals {
-  dvo = aws_acm_certificate.cert.domain_validation_options
-}
+
+
 resource "aws_route53_record" "cert_validation" {
   for_each = {
-    for dvo in tolist(aws_acm_certificate.cert.domain_validation_options) :
-    dvo.resource_record_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
+    for dvo in aws_acm_certificate.cert.domain_validation_options :
+    dvo.domain_name => dvo
   }
 
   zone_id = data.aws_route53_zone.main.zone_id
-  name    = each.value.name
-  type    = each.value.type
-  records = [each.value.record]
-  ttl     = 60
+
+  name    = each.value.resource_record_name
+  type    = each.value.resource_record_type
+  records = [each.value.resource_record_value]
+
+  ttl = 60
+
   allow_overwrite = true
 }
 
 resource "aws_acm_certificate_validation" "cert_validation" {
-  certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+  certificate_arn = aws_acm_certificate.cert.arn
+
+  validation_record_fqdns = [
+    for r in aws_route53_record.cert_validation :
+    r.fqdn
+  ]
 }
