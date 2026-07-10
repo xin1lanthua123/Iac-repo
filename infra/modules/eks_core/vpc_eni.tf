7a4255a9cb55008017ac438ff078a0f3fb3e9829
quanldl <ldlq2005@gmail.com>
2026-07-11 06:55:12 +0700

    modified:   infra/modules/eks_core/vpc_eni.tf

--- infra/modules/eks_core/vpc_eni.tf
+++ infra/modules/eks_core/vpc_eni.tf
@@ -1,12 +1,16 @@
   resource "aws_eks_addon" "vpc_cni" {
-    cluster_name = aws_eks_cluster.eks.name
-    addon_name   = "vpc-cni"
-    configuration_values = jsonencode({
-      enableNetworkPolicy = "true"
+  cluster_name = aws_eks_cluster.eks.name
 
-      env = {
-        ENABLE_PREFIX_DELEGATION = "true"
-        NETWORK_POLICY_ENFORCING_MODE = "strict"
-      }
-    })
-  }
+  addon_name    = "vpc-cni"
+  addon_version = "v1.22.3-eksbuild.1"
+
+  configuration_values = jsonencode({
+    enableNetworkPolicy          = "true"
+    networkPolicyEnforcingMode   = "strict"
+
+    env = {
+      ENABLE_PREFIX_DELEGATION = "true"
+      NETWORK_POLICY_ENFORCING_MODE = "strict"
+    }
+  })
+}
