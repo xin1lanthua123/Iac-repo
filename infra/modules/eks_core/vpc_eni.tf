6e430418ad6b45e1da7b9ec9973499911d137e8c
quanldl <ldlq2005@gmail.com>
2026-07-11 06:10:37 +0700

    modified:   infra/modules/eks_core/vpc_eni.tf

--- infra/modules/eks_core/vpc_eni.tf
+++ infra/modules/eks_core/vpc_eni.tf
@@ -1,10 +1,6 @@
   resource "aws_eks_addon" "vpc_cni" {
     cluster_name = aws_eks_cluster.eks.name
     addon_name   = "vpc-cni"
-    
-    resolve_conflicts_on_create = "OVERWRITE"
-    resolve_conflicts_on_update = "OVERWRITE"
-
     configuration_values = jsonencode({
       enableNetworkPolicy = "true"
 
