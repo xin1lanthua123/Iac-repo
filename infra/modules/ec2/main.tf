fa8a45dab02aee2c8da852f428d95e3fb85b05a8
quanldl <ldlq2005@gmail.com>
2026-08-17 16:42:44 +0700

    modified:   .github/workflows/validate-Infra-manifest.yaml         modified:   infra/modules/ec2/main.tf         modified:   infra/modules/ec2/variable.tf

--- infra/modules/ec2/main.tf
+++ infra/modules/ec2/main.tf
@@ -30,7 +30,7 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_security_group" "sonarqube_sg" {
-  name = "sonarqube-security-group"
+  name = "${var.env}-sonarqube-security-group"
 
   ingress {
     from_port   = 22
@@ -75,7 +75,7 @@ resource "aws_instance" "sonarqube" {
   user_data = file("userdata.sh")
 
   tags = {
-    Name        = "sonarqube-server"
+    Name        = "${var.env}-sonarqube-server"
     Environment = "cicd"
   }
 }
